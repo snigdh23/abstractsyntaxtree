9d61b928f251ba0dd82c65e6b8a36789f8f9711c
Snigdh Chandra <snigdh.chandra07@gmail.com>
2017-02-24 13:48:00 +0530

    Rejected commit: Default

---
+++ src/main/java/TypeUsageReference.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * Created by snigdhc on 24/2/17.
+ */
+public class TypeUsageReference {
+
+    private String typeName;
+    private List<TypeMethodCallList> methodsCalled;
+
+}
