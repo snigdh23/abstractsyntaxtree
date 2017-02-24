e50c46da9b24710920a1805a880ad2e78b9e4827
Snigdh Chandra <snigdh.chandra07@gmail.com>
2017-02-24 13:53:00 +0530

    Rejected commit: Default

---
+++ src/main/java/MethodDeclaredContext.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+/**
+ * Created by snigdhc on 24/2/17.
+ */
+public class MethodDeclaredContext {
+
+    private String declaredMethod;
+    private List<TypeUsageReference> typeList;
+
+}
